7b8c675d0dc3bde951ed43af9f48458ee24ce57f
maamokun/MikanDev <maamo-kun@outlook.com>
2024-05-01 19:57:07 +0900

    reactify theme config

--- pages/theme.config.tsx
+++ pages/theme.config.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import Image from "next/image";
 import mikanMascot from "../app/assets/MikanMascotFull.png";
 
-export default {
-    logo: (
+const ThemeConfig = () => {
+    return (
         <>
             <Image
                 src={mikanMascot.src}
@@ -14,26 +15,24 @@ export default {
             <span style={{ marginLeft: ".4em", fontWeight: 800 }}>
                 MikanDev Docs
             </span>
+            <footer>
+                <span>
+                    © 2020-{new Date().getFullYear()}{" "}
+                    <a href="/" target="_blank">
+                        MikanDev
+                    </a>
+                </span>
+            </footer>
+            <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta property="og:title" content="MikanDev Docs" />
+                <meta
+                    property="og:description"
+                    content="Documentation for MikanDev services"
+                />
+            </head>
         </>
-    ),
-    footer: {
-        text: (
-            <span>
-                © 2020-{new Date().getFullYear()}{" "}
-                <a href="/" target="_blank">
-                    MikanDev
-                </a>
-            </span>
-        ),
-    },
+    );
 };
-head: () => {
-    <>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content="MikanDev Docs" />
-        <meta
-            property="og:description"
-            content="Documentation for MikanDev services"
-        />
-    </>;
-};
+
+export default ThemeConfig;
