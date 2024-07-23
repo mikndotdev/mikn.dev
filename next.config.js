4fc9f22e31c4dee1392bc80b6d86323144a2f9ce
maamokun <maamo-kun@outlook.com>
2024-07-23 11:27:17 +0900

    remove fly, coolify is king

--- next.config.js
+++ next.config.js
@@ -26,15 +26,6 @@ const nextConfig = {
             },
         ],
     },
-    async redirects() {
-        return [
-          {
-            source: '/account',
-            destination: 'https://my.mikandev.com/',
-            permanent: true,
-          },
-        ]
-    }
 };
 
 module.exports = nextConfig;
