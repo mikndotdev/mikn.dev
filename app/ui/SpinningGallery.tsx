3787adf940a464a394b1ece84b71b2a94abd54aa
maamokun/MikanDev <maamo-kun@outlook.com>
2024-09-10 18:05:08 +0900

    fix: image array

--- app/ui/SpinningGallery.tsx
+++ app/ui/SpinningGallery.tsx
@@ -9,7 +9,7 @@ interface SpinningGalleryProps {
 }
 
 const shuffleArray = (array: any[]) => {
-    for (let i = array.length - 1; i > 0; i--) {
+    for (let i = array.length; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
