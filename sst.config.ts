c6af0676f2634556e4db75676291e7464b405ed0
maamokun/MikanDev <maamo-kun@outlook.com>
2024-09-22 20:21:37 +0900

    fix: secrets (maybe?)

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,13 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
-import { Secret } from "./.sst/platform/src/components";
+const logtoSecret = new sst.Secret("LOGTO_CLIENT_SECRET")
+const logtoId = new sst.Secret("LOGTO_CLIENT_ID")
+const logtom2msecret = new sst.Secret("LOGTO_M2M_SECRET")
+const lotom2mid = new sst.Secret("LOGTO_M2M_ID")
+const nextauthsecret = new sst.Secret("NEXTAUTH_SECRET")
+const s3endpoint = new sst.Secret("S3_ENDPOINT")
+const s3accesskey = new sst.Secret("S3_ACCESS_KEY")
+const s3secret = new sst.Secret("S3_SECRET_KEY")
 
 export default $config({
   app(input) {
@@ -13,7 +20,7 @@ export default $config({
   },
   async run() {
     new sst.aws.Nextjs("mikn-dev" , {
-      link: [Secret],
+      link: [logtoSecret, logtoId, logtom2msecret, lotom2mid, nextauthsecret, s3endpoint, s3accesskey, s3secret],
       domain: {
         name: "mikn.dev",
         dns: sst.cloudflare.dns()
