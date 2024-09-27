77907fb35956275aa2d69fbb306c02a708b202d9
maamokun <maamo-kun@outlook.com>
2024-09-27 13:26:23 +0900

    add some stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useClientTranslation } from "@/app/i18n/client";
 import { Button, Heading, Card, Center, Flex } from "@neodyland/ui";
+import { cookieName } from "./i18n/settings";
 import { useRouter } from "next/navigation";
 
 import Image from "next/image";
