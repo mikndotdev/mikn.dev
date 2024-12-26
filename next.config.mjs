2c6df62a3c138c3f337bba23f92b5f2266341d7b
maamokun/MikanDev <maamo-kun@outlook.com>
2024-12-26 20:02:27 +0900

    WIP: full refactor with Next v15

---
+++ next.config.mjs
@@ -0,0 +1,39 @@
+import createNextIntlPlugin from "next-intl/plugin";
+
+/** @type {import('next').NextConfig} */
+
+const withNextIntl = createNextIntlPlugin();
+
+const nextConfig = {
+    images: {
+        dangerouslyAllowSVG: true,
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "cdn.mikn.dev",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "cdn.mdusercontent.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "cdn.discordapp.com",
+                port: "",
+                pathname: "/avatars/**",
+            },
+            {
+                protocol: "https",
+                hostname: "cdn.statically.io",
+                port: "",
+                pathname: "/avatars/*",
+            },
+        ],
+    },
+};
+
+export default withNextIntl(nextConfig);
