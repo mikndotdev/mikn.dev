cb918609ec08f07acf7dfe81df47f96d81bf17cd
maamokun/MikanDev <maamo-kun@outlook.com>
2024-12-26 21:26:01 +0900

    idk

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "MikanDev",
+    description: "We make cool stuff to make life easier 🍊",
+    openGraph: {
+        images: ["https://mikn.dev/og-homepage.png"],
+    },
+};
+
+export default function RootLayout({ children } : { children: ReactNode }) {
+    return (
+        <>
+            {children}
+        </>
+    )
+}
