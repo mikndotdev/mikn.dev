19e52b601183d44ca901006de9093831ef8239e9
maamokun/MikanDev <maamo-kun@outlook.com>
2025-03-26 21:02:48 +0900

    feat: Dev mode uses Tolgee API

--- i18n/request.ts
+++ i18n/request.ts
@@ -10,6 +10,21 @@ export default getRequestConfig(async ({ requestLocale }) => {
 		locale = routing.defaultLocale;
 	}
 
+	if (process.env.NODE_ENV === 'development') {
+		const res = await fetch(`${process.env.TOLGEE_API_URL}/v2/projects/${process.env.TOLGEE_PROJECT_ID}/translations/${locale}`, {
+			headers: {
+				'x-api-key': `${process.env.TOLGEE_API_KEY}`,
+			},
+		});
+		const data = await res.json();
+		const messages = data[locale];
+
+		return {
+			locale,
+			messages,
+		};
+	}
+
 	const res = await fetch(`${process.env.I18N_PUBLIC_URL}/${locale}.json`);
 
 	return {
