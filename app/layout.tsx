60a706341a234dd7e062ea1805e3bd4916cba67d
maamokun/MikanDev <maamo-kun@outlook.com>
2025-03-26 19:18:53 +0900

    feat: Metadata Files

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,6 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
 	title: "MikanDev",
 	description: "We make cool stuff to make life easier 🍊",
-	openGraph: {
-		images: ["https://mikn.dev/og-homepage.png"],
-	},
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,7 @@ import { routing } from "@/i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-	// matcher: '/:lng*'
 	matcher: [
-		"/((?!api|_next/static|_next/image|img|favicon.ico|sw.js|sitemap.xml|vroid*|98gas9mkeb.txt|robots.txt|og-homepage.png).*)",
+		"/((?!api|_next/static|_next/image|img|favicon.ico|sw.js|sitemap.xml|vroid*|98gas9mkeb.txt|robots.txt|opengraph-image.png).*)",
 	],
 };
