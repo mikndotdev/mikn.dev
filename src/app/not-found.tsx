48439e4e989e4e932e69d69e4f700be3319c5315
maamokun/MikanDev <sachadeco214@gmail.com>
2025-05-27 23:11:34 +0900

    fix: files missing from git

---
+++ src/app/not-found.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const hsr = localFont({ src: ".././assets/fonts/HSR.woff2" });
+
+export default function NotFound() {
+	return (
+		<>
+			<html className={hsr.className} lang="en">
+				<body>
+					<div className="h-screen flex flex-col items-center justify-center">
+						<h1 className={"text-4xl text-center text-white"}>
+							404 - Page Not Found
+						</h1>
+						<Link href={"/"}>
+							<button
+								className={"btn btn-primary text-white mt-5"}
+							>
+								Take me back
+							</button>
+						</Link>
+					</div>
+				</body>
+			</html>
+		</>
+	);
+}
