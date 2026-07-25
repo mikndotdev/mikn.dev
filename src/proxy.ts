a8fd480f6e855452e24331a5567b26211ba8c0a3
maamokun <sachadeco214@gmail.com>
2026-07-26 03:10:23 +0900

    feat: Ads.txt

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,5 +4,5 @@ import { routing } from "@/i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: "/((?!api|img|_next|_vercel|.*\\..*).*)",
+  matcher: "/((?!api|img|_next|_vercel|ads.txt|.*\\..*).*)",
 };
